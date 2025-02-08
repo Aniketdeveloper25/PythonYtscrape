9c91de04593d0efadc2b76f060bbb0cb29159e6a
Aniket <50550752+Aniketdeveloper25@users.noreply.github.com>
2025-02-08 18:20:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,9 +55,22 @@ def get_country_full_name(country_code):
 
 def write_to_google_sheet(data, sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        r'\credentials.json', scope)  # Add Your JSON file Path Here...
-    client = gspread.authorize(creds)  # Use 'client' for clarity
+    
+    creds_dict = {
+        "type": "service_account",
+        "project_id": os.environ.get('GOOGLE_PROJECT_ID'),
+        "private_key_id": os.environ.get('GOOGLE_PRIVATE_KEY_ID'),
+        "private_key": os.environ.get('GOOGLE_PRIVATE_KEY').replace('\\n', '\n'),
+        "client_email": os.environ.get('GOOGLE_CLIENT_EMAIL'),
+        "client_id": os.environ.get('GOOGLE_CLIENT_ID'),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{os.environ.get('GOOGLE_CLIENT_EMAIL')}"
+    }
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
     try:
         sheet = client.open(sheet_name).sheet1
     except gspread.exceptions.SpreadsheetNotFound:
